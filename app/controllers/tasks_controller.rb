88d7f310c59f02d5e0668e99f6721525db552502
sebasong <sebasong@yahoo.com>
2016-07-21 09:24:47 +0000

    Added states to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,9 @@ before_action :authenticate_user!
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = current_user.tasks
+     @to_do = current_user.tasks.where(state: "to_do")
+    @doing = current_user.tasks.where(state: "doing")
+    @done = current_user.tasks.where(state: "done")
   end
 
   # GET /tasks/1
@@ -69,6 +71,6 @@ before_action :authenticate_user!
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content,:state)
     end
 end
